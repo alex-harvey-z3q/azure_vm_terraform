da9684376b6b2bfb89fec005111d8093f128a039
Alex Harvey <alexharv074@gmail.com>
2026-03-19 10:31:32 +1100

    Refactor variables to use locals pattern

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,62 +13,52 @@ provider "azurerm" {
   features {}
 }
 
-variable "location" {
-  type    = string
-  default = "australiasoutheast"
-}
+locals {
+  location            = "australiasoutheast"
+  resource_group_name = "rg-personal-ansible-api"
 
-variable "resource_group_name" {
-  type    = string
-  default = "rg-personal-ansible-api"
-}
+  vm_name        = "vm-personal-ansible-api"
+  admin_username = "azureuser"
 
-variable "vm_name" {
-  type    = string
-  default = "vm-personal-ansible-api"
-}
-
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
+  ssh_public_key = var.ssh_public_key
 }
 
 variable "ssh_public_key" {
   type        = string
-  description = "Your SSH public key contents, e.g. ssh-ed25519 AAAA..."
+  description = "Your SSH public key contents"
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = local.resource_group_name
+  location = local.location
 }
 
 resource "azurerm_virtual_network" "this" {
   name                = "vnet-personal-ansible-api"
   address_space       = ["10.10.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
 }
 
 resource "azurerm_subnet" "this" {
   name                 = "snet-default"
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = local.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.10.1.0/24"]
 }
 
 resource "azurerm_public_ip" "this" {
   name                = "pip-personal-ansible-api"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "this" {
   name                = "nsg-personal-ansible-api"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
 
   security_rule {
     name                       = "allow-ssh"
@@ -97,8 +87,8 @@ resource "azurerm_network_security_group" "this" {
 
 resource "azurerm_network_interface" "this" {
   name                = "nic-personal-ansible-api"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -114,11 +104,11 @@ resource "azurerm_network_interface_security_group_association" "this" {
 }
 
 resource "azurerm_linux_virtual_machine" "this" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  name                = local.vm_name
+  resource_group_name = local.resource_group_name
+  location            = local.location
   size                = "Standard_D2s_v3"
-  admin_username      = var.admin_username
+  admin_username      = local.admin_username
 
   disable_password_authentication = true
 
@@ -127,12 +117,12 @@ resource "azurerm_linux_virtual_machine" "this" {
   ]
 
   admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
+    username   = local.admin_username
+    public_key = local.ssh_public_key
   }
 
   os_disk {
-    name                 = "osdisk-${var.vm_name}"
+    name                 = "osdisk-${local.vm_name}"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -154,5 +144,5 @@ output "public_ip" {
 }
 
 output "ssh_command" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.this.ip_address}"
+  value = "ssh ${local.admin_username}@${azurerm_public_ip.this.ip_address}"
 }
