d6c501d54848731ff7f42180d4e9c6f42b1a40d5
Alex Harvey <alexharv074@gmail.com>
2026-03-19 10:50:29 +1100

    Add Windows VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,15 +17,32 @@ locals {
   location            = "australiasoutheast"
   resource_group_name = "rg-personal-ansible-api"
 
-  vm_name        = "vm-personal-ansible-api"
-  admin_username = "azureuser"
+  vnet_name   = "vnet-personal-ansible-api"
+  subnet_name = "snet-default"
 
-  ssh_public_key = var.ssh_public_key
+  linux_vm_name        = "vm-personal-ansible-api"
+  linux_admin_username = "azureuser"
+  linux_vm_size        = "Standard_D2s_v3"
+
+  windows_vm_name        = "vm-personal-windows"
+  windows_admin_username = "azureuser"
+  windows_vm_size        = "Standard_D2s_v3"
+
+  address_space = ["10.10.0.0/16"]
+  subnet_prefix = ["10.10.1.0/24"]
+
+  ssh_private_key_path = "~/.ssh/id_ed25519"
 }
 
 variable "ssh_public_key" {
   type        = string
   description = "Your SSH public key contents"
+}
+
+variable "windows_admin_password" {
+  type        = string
+  sensitive   = true
+  description = "Administrator password for the Windows VM"
 }
 
 resource "azurerm_resource_group" "this" {
@@ -34,31 +51,39 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_virtual_network" "this" {
-  name                = "vnet-personal-ansible-api"
-  address_space       = ["10.10.0.0/16"]
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  name                = local.vnet_name
+  address_space       = local.address_space
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "azurerm_subnet" "this" {
-  name                 = "snet-default"
-  resource_group_name  = local.resource_group_name
+  name                 = local.subnet_name
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.10.1.0/24"]
+  address_prefixes     = local.subnet_prefix
 }
 
-resource "azurerm_public_ip" "this" {
+resource "azurerm_public_ip" "linux" {
   name                = "pip-personal-ansible-api"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_public_ip" "windows" {
+  name                = "pip-personal-windows"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "this" {
   name                = "nsg-personal-ansible-api"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
   security_rule {
     name                       = "allow-ssh"
@@ -83,46 +108,88 @@ resource "azurerm_network_security_group" "this" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "allow-rdp"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-winrm-https"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-resource "azurerm_network_interface" "this" {
+resource "azurerm_network_interface" "linux" {
   name                = "nic-personal-ansible-api"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
   ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.this.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.this.id
+    public_ip_address_id          = azurerm_public_ip.linux.id
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "this" {
-  network_interface_id      = azurerm_network_interface.this.id
+resource "azurerm_network_interface" "windows" {
+  name                = "nic-personal-windows"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.windows.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "linux" {
+  network_interface_id      = azurerm_network_interface.linux.id
   network_security_group_id = azurerm_network_security_group.this.id
 }
 
-resource "azurerm_linux_virtual_machine" "this" {
-  name                = local.vm_name
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  size                = "Standard_D2s_v3"
-  admin_username      = local.admin_username
+resource "azurerm_network_interface_security_group_association" "windows" {
+  network_interface_id      = azurerm_network_interface.windows.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_linux_virtual_machine" "linux" {
+  name                = local.linux_vm_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  size                = local.linux_vm_size
+  admin_username      = local.linux_admin_username
 
   disable_password_authentication = true
 
   network_interface_ids = [
-    azurerm_network_interface.this.id
+    azurerm_network_interface.linux.id
   ]
 
   admin_ssh_key {
-    username   = local.admin_username
-    public_key = local.ssh_public_key
+    username   = local.linux_admin_username
+    public_key = var.ssh_public_key
   }
 
   os_disk {
-    name                 = "osdisk-${local.vm_name}"
+    name                 = "osdisk-${local.linux_vm_name}"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -135,14 +202,57 @@ resource "azurerm_linux_virtual_machine" "this" {
   }
 }
 
+resource "azurerm_windows_virtual_machine" "windows" {
+  name                = local.windows_vm_name
+  computer_name       = "winpoc01"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  size                = local.windows_vm_size
+  admin_username      = local.windows_admin_username
+  admin_password      = var.windows_admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.windows.id
+  ]
+
+  os_disk {
+    name                 = "osdisk-${local.windows_vm_name}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+}
+
 output "resource_group_name" {
   value = azurerm_resource_group.this.name
 }
 
-output "public_ip" {
-  value = azurerm_public_ip.this.ip_address
+output "linux_public_ip" {
+  value = azurerm_public_ip.linux.ip_address
+}
+
+output "linux_private_ip" {
+  value = azurerm_network_interface.linux.private_ip_address
 }
 
 output "ssh_command" {
-  value = "ssh ${local.admin_username}@${azurerm_public_ip.this.ip_address}"
+  value = "ssh -i ${local.ssh_private_key_path} ${local.linux_admin_username}@${azurerm_public_ip.linux.ip_address}"
+}
+
+output "windows_public_ip" {
+  value = azurerm_public_ip.windows.ip_address
+}
+
+output "windows_private_ip" {
+  value = azurerm_network_interface.windows.private_ip_address
+}
+
+output "windows_admin_username" {
+  value = local.windows_admin_username
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,1 +1,2 @@
-ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfpKcJdyfKKKWv7tqjYr85AD5ThNPJ+NLjqXRQcUfrC alexharvey@Alexs-MacBook-Pro-2.local"
+ssh_public_key         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfpKcJdyfKKKWv7tqjYr85AD5ThNPJ+NLjqXRQcUfrC alexharvey@Alexs-MacBook-Pro-2.local"
+windows_admin_password = "ABcd1234!"
